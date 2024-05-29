87d541836d5e035d3d6f7182ac1e4a6c0a2ee995
emanuele <emanuele971@icloud.com>
2024-05-29 19:04:19 +0200

    connection login  and calendar works

--- back/api/index.js
+++ back/api/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const userRoutes = require("./routes/user.routes");
-require("./config/connection"); // Importa la connessione al database
+const userRoutes = require("../routes/user.routes");
+require("../config/connection"); // Importa la connessione al database
 const cors = require("cors");
 
 const app = express();
+const domain = process.env.DOMAIN;
+const test = process.env.DOMAIN_TEST;
 
 // Middleware per processare JSON
 app.use(bodyParser.json());
 
 // Middleware per abilitare CORS
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: [test, domain],
+  })
+);
 
 // Usa le rotte definite in userRoutes
 app.use("/api/users", userRoutes);
